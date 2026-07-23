0b9a9068ad4989176c2984e3dde1a7e1f434d60d
Antonio Lo Giudice <a.logiudice@reply.it>
2026-07-23 13:11:48 +0000

    .

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -5,9 +5,22 @@
 
     from dbt-tutorial.jaffle_shop.customers*/
 
-    select
+with 
+
+source as (
+    
+    select * 
+    from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select 
         id as customer_id,
         first_name,
         last_name
+    from source
+)
 
-    from {{ source('jaffle_shop', 'customers') }}
+select * from renamed
